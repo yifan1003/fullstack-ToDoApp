a0d9768973cea6e6d74c106a6f5679434b470578
yifan1003 <hyifan9988@gmail.com>
2021-06-09 00:28:14 -0700

    Complete frontend

---
+++ TodoAppBackend/src/test/java/com/yifan/demo/TodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
